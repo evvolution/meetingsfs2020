b38eabfaf77aded0d92eaadf8e752fbb855c6c59
evvolution <zhouxiaoemail@163.com>
2020-02-12 01:50:37 +0800

    new

--- js/scaleToWindow.js
+++ js/scaleToWindow.js
@@ -16,36 +16,36 @@ function scaleToWindow(canvas, backgroundColor) {
     //Wide canvases should be centered vertically, and 
     //square or tall canvases should be centered horizontally
     if (canvas.offsetWidth > canvas.offsetHeight) {
-        if (canvas.offsetWidth * scale < window.innerWidth) {
-            center = "horizontally";
-        } else {
-            center = "vertically";
-        }
+      if (canvas.offsetWidth * scale < window.innerWidth) {
+        center = "horizontally";
+      } else {
+        center = "vertically";
+      }
     } else {
-        if (canvas.offsetHeight * scale < window.innerHeight) {
-            center = "vertically";
-        } else {
-            center = "horizontally";
-        }
+      if (canvas.offsetHeight * scale < window.innerHeight) {
+        center = "vertically";
+      } else {
+        center = "horizontally";
+      }
     }
   
     //Center horizontally (for square or tall canvases)
     var margin;
     if (center === "horizontally") {
-        margin = (window.innerWidth - canvas.offsetWidth * scale) / 2;
-        canvas.style.marginTop = 0 + "px";
-        canvas.style.marginBottom = 0 + "px";
-        canvas.style.marginLeft = margin + "px";
-        canvas.style.marginRight = margin + "px";
+      margin = (window.innerWidth - canvas.offsetWidth * scale) / 2;
+      canvas.style.marginTop = 0 + "px";
+      canvas.style.marginBottom = 0 + "px";
+      canvas.style.marginLeft = margin + "px";
+      canvas.style.marginRight = margin + "px";
     }
   
     //Center vertically (for wide canvases) 
     if (center === "vertically") {
-        margin = (window.innerHeight - canvas.offsetHeight * scale) / 2;
-        canvas.style.marginTop = margin + "px";
-        canvas.style.marginBottom = margin + "px";
-        canvas.style.marginLeft = 0 + "px";
-        canvas.style.marginRight = 0 + "px";
+      margin = (window.innerHeight - canvas.offsetHeight * scale) / 2;
+      canvas.style.marginTop = margin + "px";
+      canvas.style.marginBottom = margin + "px";
+      canvas.style.marginLeft = 0 + "px";
+      canvas.style.marginRight = 0 + "px";
     }
   
     //3. Remove any padding from the canvas  and body and set the canvas
@@ -62,16 +62,16 @@ function scaleToWindow(canvas, backgroundColor) {
     //Fix some quirkiness in scaling for Safari
     var ua = navigator.userAgent.toLowerCase();
     if (ua.indexOf("safari") != -1) {
-        if (ua.indexOf("chrome") > -1) {
+      if (ua.indexOf("chrome") > -1) {
         // Chrome
-        } else {
+      } else {
         // Safari
         //canvas.style.maxHeight = "100%";
         //canvas.style.minHeight = "100%";
-        }
+      }
     }
   
     //5. Return the `scale` value. This is important, because you'll nee this value 
     //for correct hit testing between the pointer and sprites
     return scale;
-}
+  }
